235d3a25be16aa89001b9e1a44147253cda2f295
Bethany Drake <drake_beth@yahoo.com.au>
2021-07-07 00:33:02 +1000

    only highlight enabled files

--- extension.js
+++ extension.js
@@ -21,21 +21,37 @@ function activate(context) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "highlighter" is now active!');
 
+	var settings = vscode.workspace.getConfiguration('highlighter');
+
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('highlighter.helloWorld', function () {
 		// The code you place here will be executed every time your command is executed
-
+		
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from highlighter!');
 	});
 
+	let addFileCommand = vscode.commands.registerCommand('highlighter.addFile', function () {
+		
+		var currentFile = vscode.window.activeTextEditor.document.fileName;
+		var oldFiles = settings.get('enabledFiles') ?? []; 
+		oldFiles.push(currentFile)
+		settings.update('enabledFiles', oldFiles)
+		
+		vscode.window.showInformationMessage('Enabled highlighting for file: '+ currentFile);
+	});
+
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(addFileCommand);
+	
 	vscode.window.onDidChangeActiveTextEditor(updateDecorations, null, context.subscriptions);
 
 	function updateDecorations(activeEditor) {
 
+		if (!settings.get('enabledFiles').includes(activeEditor.document.fileName)) return;
+		
 		try {
 			var text = activeEditor.document.getText();
 
